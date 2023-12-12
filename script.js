99b05b62871d9c6a78777938153a6c74141a0952
Clément Toquet <toquet.clement@gmail.com>
2023-12-12 19:31:23 +0100

    add style and toggle to question choice

--- script.js
+++ script.js
@@ -23,27 +23,27 @@ const quizData = [
     {
         id: 1,
         question: "1ère question",
-        responses: ["1", "2", "3", "4"],
-        correctResponse: "2"
+        responses: ["1 premiere question", "2 premiere question", "3 premiere question", "4 premiere question"],
+        correctResponse: "2 premiere question"
     },
     {
         id: 2,
         question: "2e question",
-        responses: ["1", "2", "3", "4"],
-        correctResponse: "2"
+        responses: ["1 deuxième question", "2 deuxième question", "3 deuxième question", "4 deuxième question"],
+        correctResponse: "2 deuxième question"
     },
     {
         id: 3,
         question: "3e Question",
-        responses: ["1", "2", "3", "4"],
-        correctResponse: "2"
+        responses: ["1 troisième question", "2 troisième question", "3 troisième question", "4 troisième question"],
+        correctResponse: "2 troisième question"
 
     },
     {
         id: 4,
         question: "4e Question",
-        responses: ["1", "2", "3", "4"],
-        correctResponse: "2"
+        responses: ["1 quatrième question", "2 quatrième question", "3 quatrième question", "4 quatrième question"],
+        correctResponse: "2 quatrième question"
     }
 ];
 
@@ -69,20 +69,20 @@ btnNextResponses.addEventListener("click", function(e) {
     const currentQuestion = e.target.dataset.question;
     if(currentQuestion < quizLength) {
         e.target.dataset.question++; // add one to go to next question number
+        addQuestionTitleToDOM(e.target.dataset.question, questionTitle);
+        addQuestionToDOM(e.target.dataset.question, responsesList);
+
+    } else {
+        // alert("La partie est finie !!");
+        console.log(document.querySelector("#results"));
+        document.querySelector("#results").style.display = "inline";
     }
 })
 
 
 
-
-
 // stocker la réponse du user, la bonne réponse, l'état de la réponse
-
 const userResponses = [] // array of user responses, values added with Map Object
-
-
-
-
 
 
 addQuestionTitleToDOM(currentQuestion, questionTitle)
@@ -100,15 +100,22 @@ function addQuestionTitleToDOM(currentQuestion, questionTitle) {
 
 
 function addQuestionToDOM(currentQuestion, responsesList) {   
-    responsesList.dataset.correctAnswer
+    responsesList.innerHTML = ""; // reset the responses list
+    responsesList.dataset.correctAnswer;
     quizData[currentQuestion].responses.forEach((quizResponse) => {
         const li = document.createElement("li");
-        const cls = ["col-6", "px-3", "py-5", , "bg", "bg-light", "border", "border-primary-subtle"];
+        const cls = ["response", "col-6", "px-3", "py-5", , "bg", "bg-light", "border", "border-primary-subtle"];
         li.classList.add(...cls);
         li.textContent = quizResponse;
         responsesList.appendChild(li);
-        li.addEventListener("click", function() {
-
+        li.addEventListener("click", function(e) {
+            if(document.querySelector(".selected")) {
+                document.querySelector(".selected").classList.remove("selected");
+                console.log("dans le if");
+                console.log(document.querySelector(".selected"));
+            }
+            console.log(e.target);
+            e.target.classList.add("selected");
         });
     });
 
