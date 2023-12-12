2c97ac3a5e5d8be1aee0f71b1dbb5d51cdd4266b
Clément Toquet <toquet.clement@gmail.com>
2023-12-12 15:36:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Quiz interactif
+// Objectif thématique : Manipulations sur tableaux, Boucles forEach, Fonctions callback, Objet
+// map
+// 1. Préparation des données
+// ● Créer un tableau multi-dimensionnel avec des questions, options de réponse
+// et la réponse correcte pour un quiz sur le sujet que vous souhaitez.
+// 2. Affichage du quiz
+// ● Utiliser forEach pour itérer sur le tableau et afficher chaque question avec les
+// options de réponse.
+// 3. Gestion des réponses
+// ● Utiliser un objet map pour stocker les réponses des utilisateurs avec le format
+// { questionId: réponse }.
+// 4. Calcul des résultats
+// ● Utiliser une fonction callback pour vérifier les réponses correctes et calculer le
+// score final.
+
+
+
+// Objet Map : Un objet Map contient des paires de clé-valeur et mémorise l'ordre dans lequel les clés ont été insérées.
+
+
+
+
+
