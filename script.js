73788185e61580249b625f825aec7dc3361a5713
Clément Toquet <toquet.clement@gmail.com>
2023-12-13 10:13:26 +0100

    quiz working !! show correct responses and score of user

--- script.js
+++ script.js
@@ -21,31 +21,31 @@
 
 // const quizData = [
 //     {
-//         id: 1,
+//         id: 0,
 //         question: "Quel organisme est capable de survivre dans l'espace ?",
 //         responses: ["Fourmis", "Tardigrades", "Bactéries E. coli", "Araignées"],
 //         correctResponse: "Tardigrades"
 //     },
 //     {
-//         id: 2,
+//         id: 1,
 //         question: "Quel animal peut régénérer la plupart de ses organes, y compris son cerveau ?",
 //         responses: ["Requin", "Méduse", "Étoile de mer", "Lézard"],
 //         correctResponse: "Étoile de mer"
 //     },
 //     {
-//         id: 3,
+//         id: 2,
 //         question: "Quel est le seul mammifère capable de voler ?",
 //         responses: ["Chauve-souris", "Souris", "Oiseau-mouche", "Écureuil"],
 //         correctResponse: "Chauve-souris"
 //     },
 //     {
-//         id: 4,
+//         id: 3,
 //         question: "Quel est le plus petit os du corps humain ?",
 //         responses: ["Os du bras", "Os de la cuisse", "Os de l'oreille", "Os du nez"],
 //         correctResponse: "Os de l'oreille"
 //     },
 //     {
-//         id: 5,
+//         id: 4,
 //         question: "Quel poisson électrique peut produire une décharge électrique suffisamment puissante pour allumer une ampoule ?",
 //         responses: ["Poisson-globe", "Raie électrique", "Anguille électrique", "Poisson-chat électrique"],
 //         correctResponse: "Anguille électrique"
@@ -79,30 +79,54 @@
 
 
 const quizData = [
+    // {
+    //     id: 0,
+    //     question: "1ère question",
+    //     responses: ["1 premiere question", "2 premiere question", "3 premiere question", "4 premiere question"],
+    //     correctResponse: "2 premiere question"
+    // },
+    // {
+    //     id: 1,
+    //     question: "2e question",
+    //     responses: ["1 deuxième question", "2 deuxième question", "3 deuxième question", "4 deuxième question"],
+    //     correctResponse: "2 deuxième question"
+    // },
+    // {
+    //     id: 2,
+    //     question: "3e Question",
+    //     responses: ["1 troisième question", "2 troisième question", "3 troisième question", "4 troisième question"],
+    //     correctResponse: "2 troisième question"
+
+    // },
+    // {
+    //     id: 3,
+    //     question: "4e Question",
+    //     responses: ["1 quatrième question", "2 quatrième question", "3 quatrième question", "4 quatrième question"],
+    //     correctResponse: "2 quatrième question"
+    // }
+    {
+        id: 0,
+        question: "Quel organisme est capable de survivre dans l'espace ?",
+        responses: ["Fourmis", "Tardigrades", "Bactéries E. coli", "Araignées"],
+        correctResponse: "Tardigrades"
+    },
     {
         id: 1,
-        question: "1ère question",
-        responses: ["1 premiere question", "2 premiere question", "3 premiere question", "4 premiere question"],
-        correctResponse: "2 premiere question"
+        question: "Quel animal peut régénérer la plupart de ses organes, y compris son cerveau ?",
+        responses: ["Requin", "Méduse", "Étoile de mer", "Lézard"],
+        correctResponse: "Étoile de mer"
     },
     {
         id: 2,
-        question: "2e question",
-        responses: ["1 deuxième question", "2 deuxième question", "3 deuxième question", "4 deuxième question"],
-        correctResponse: "2 deuxième question"
+        question: "Quel est le seul mammifère capable de voler ?",
+        responses: ["Chauve-souris", "Souris", "Oiseau-mouche", "Écureuil"],
+        correctResponse: "Chauve-souris"
     },
     {
         id: 3,
-        question: "3e Question",
-        responses: ["1 troisième question", "2 troisième question", "3 troisième question", "4 troisième question"],
-        correctResponse: "2 troisième question"
-
-    },
-    {
-        id: 4,
-        question: "4e Question",
-        responses: ["1 quatrième question", "2 quatrième question", "3 quatrième question", "4 quatrième question"],
-        correctResponse: "2 quatrième question"
+        question: "Quel est le plus petit os du corps humain ?",
+        responses: ["Os du bras", "Os de la cuisse", "Os de l'oreille", "Os du nez"],
+        correctResponse: "Os de l'oreille"
     }
 ];
 
@@ -128,28 +152,25 @@ const responsesList = document.querySelector("#questionResponses");
 const btnNextResponses = document.querySelector("#next");
 btnNextResponses.dataset.question = currentQuestion;
 
-
-// stocker la réponse du user, la bonne réponse, l'état de la réponse
-//const userResponses = []; // array of user responses, values added with Map Object
-const userResponses = new Map(); // array of user responses, values added with Map Object
+// map object to store the responses of the User
+const userResponses = new Map(); 
 
 
 // initialize game in the DOM for the first question
-addQuestionTitleToDOM(currentQuestion, questionTitle)
+addQuestionTitleToDOM(currentQuestion, questionTitle);
 addQuestionToDOM(currentQuestion, responsesList);
+
 
 
 // 
 btnNextResponses.addEventListener("click", function(e) {
-    console.log(e.target.dataset.question);
     const currentQuestion = e.target.dataset.question;
-    if(currentQuestion < quizLength - 1) {
+    if(currentQuestion < quizLength) {
         e.target.dataset.question++; // add one to go to next question number
         addQuestionTitleToDOM(e.target.dataset.question, questionTitle);
         addQuestionToDOM(e.target.dataset.question, responsesList);
 
     } else {
-        // alert("La partie est finie !!");
         e.target.setAttribute("disabled", true);
         console.log(document.querySelector("#results"));
         document.querySelector("#results").style.display = "inline";
@@ -157,8 +178,40 @@ btnNextResponses.addEventListener("click", function(e) {
 })
 
 
+const btnShowResults = document.querySelector("#results");
+
+// create elements and get correct responses of the quiz
+btnShowResults.addEventListener("click", function() {
+    // console.log(userResponses)
+    const resultsList = document.querySelector("#resultsList");
+    const iterator = userResponses.entries();
+    // console.log(iterator);
+    const length = userResponses.size;
+
+    let quizScore = 0;
+    for(let i = 0; i < length; i++) {
+        const userResponse = iterator.next().value;
+        // console.log(userResponse);
+        const li = document.createElement("li");
+        if(userResponses.get(i) == quizData[i].correctResponse) {
+            quizScore++;
+        } else {
+            // console.warn("Tu n'as pas choisi la bonne réponse");
+        }
 
 
+
+        li.innerHTML = `Question n°${i} :<br/>  Réponse utilisateur : ${userResponses.get(i)} <br/> Bonne réponse : ${quizData[i].correctResponse} <br/><br/>`;
+        resultsList.appendChild(li);
+
+    }
+
+    const score = document.createElement("h3");
+    score.innerText = `Ton score est de : ${quizScore}`;
+    document.querySelector("#resultsContainer").appendChild(score);
+
+    
+});
 
 
 
@@ -166,12 +219,21 @@ btnNextResponses.addEventListener("click", function(e) {
 // ----------------------------------------------------
 // FUNCTIONS
 
+
+function checkAnswers() {
+
+}
+
+
 function addQuestionTitleToDOM(currentQuestion, questionTitle) {
+    if(quizData[currentQuestion] == undefined) return;
     questionTitle.innerText = quizData[currentQuestion].question;
 }
 
 
 function addQuestionToDOM(currentQuestion, responsesList) {   
+    if(quizData[currentQuestion] == undefined) return;
+
     responsesList.innerHTML = ""; // reset the responses list
     responsesList.dataset.correctAnswer;
     quizData[currentQuestion].responses.forEach((quizResponse) => {
@@ -183,12 +245,14 @@ function addQuestionToDOM(currentQuestion, responsesList) {
         li.addEventListener("click", function(e) {
             if(document.querySelector(".selected")) {
                 document.querySelector(".selected").classList.remove("selected");
-                // console.log("dans le if");
-                // console.log(document.querySelector(".selected"));
+                
             }
-            // console.log(e.target);
+            // add css class selected
             e.target.classList.add("selected");
-            // userResponses.
+
+            // add user response to Map Object
+            userResponses.set(parseInt(currentQuestion), e.target.textContent);
+
         });
     });
 
